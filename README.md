15adac076c575dd279a6694358529a79be1adc4d
Trevin Teacutter <majora.incarnate@gmail.com>
2024-01-29 10:23:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MWO Helper
+
+Meant to be a one-stop shop for MWO comp related activities, without the need for a website.
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE.md') as f:
+    license = f.read()
+
+setup(
+    name='mwo-helper',
+    version='0.1.0',
+    description='Meant to be a one-stop shop for MWO comp related activities, without the need for a website.',
+    long_description=readme,
+    author='Trevin Teacutter',
+    author_email='tjteacutter1@cougars.ccis.edu',
+    url='https://github.com/trevinteacutter/mwo-helper',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
