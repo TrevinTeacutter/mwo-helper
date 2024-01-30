c94281ffeddf7b5d2858fe6b640c955dc8303163
Trevin Teacutter <tjteacutter1@cougars.ccis.edu>
2024-01-29 20:09:02 -0600

    klhdfa

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
     selector: 'app-match',
     templateUrl: './match.component.html',
     styleUrls: ['./match.component.scss'],
     standalone: true,
-    imports: [TranslateModule]
+    imports: [TranslateModule, ReactiveFormsModule],
 })
 export class MatchComponent implements OnInit {
+
+  matchSubmit = new FormGroup({
+    apiKey: new FormControl(''),
+    matchID: new FormControl(''),
+  });
 
   constructor() { }
 
   ngOnInit(): void {
     console.log('DetailComponent INIT');
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.matchSubmit.value);
   }
 
 }
